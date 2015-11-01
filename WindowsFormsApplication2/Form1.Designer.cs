7db9fb36989efa4c10f3f7917f50974d6f65a623
MOMANI1 <mmn1078@gmail.com>
2015-11-01 14:45:26 +0200

    I added the whole project code to the reposotary

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace LaptopStore
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.metroToggle1 = new MetroFramework.Controls.MetroToggle();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(20, 60);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.metroButton1);
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox3);
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.metroToggle1);
+            this.splitContainer1.Panel1.Controls.Add(this.metroLabel2);
+            this.splitContainer1.Panel1.Controls.Add(this.metroLabel1);
+            this.splitContainer1.Panel1.Controls.Add(this.metroComboBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(587, 372);
+            this.splitContainer1.SplitterDistance = 175;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroButton1.DisplayFocus = true;
+            this.metroButton1.Location = new System.Drawing.Point(47, 262);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(89, 23);
+            this.metroButton1.Style = MetroFramework.MetroColorStyle.Lime;
+            this.metroButton1.TabIndex = 4;
+            this.metroButton1.Text = "&Contributors ";
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.pictureBox3.Image = global::LaptopStore.Properties.Resources.decriotion1;
+            this.pictureBox3.Location = new System.Drawing.Point(3, 166);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(34, 32);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.pictureBox2.Image = global::LaptopStore.Properties.Resources.lap;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 74);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(34, 32);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // metroToggle1
+            // 
+            this.metroToggle1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.metroToggle1.AutoSize = true;
+            this.metroToggle1.Location = new System.Drawing.Point(92, 168);
+            this.metroToggle1.Name = "metroToggle1";
+            this.metroToggle1.Size = new System.Drawing.Size(80, 17);
+            this.metroToggle1.TabIndex = 2;
+            this.metroToggle1.Text = "Off";
+            this.metroToggle1.UseSelectable = true;
+            this.metroToggle1.CheckedChanged += new System.EventHandler(this.metroToggle1_CheckedChanged);
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(0, 144);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(126, 19);
+            this.metroLabel2.TabIndex = 1;
+            this.metroLabel2.Text = "Detailed Description";
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(0, 45);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(175, 19);
+            this.metroLabel1.TabIndex = 1;
+            this.metroLabel1.Text = "Choose Laptop manufacture";
+            // 
+            // metroComboBox1
+            // 
+            this.metroComboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroComboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.RecentlyUsedList;
+            this.metroComboBox1.FormattingEnabled = true;
+            this.metroComboBox1.ItemHeight = 23;
+            this.metroComboBox1.Items.AddRange(new object[] {
+            "Appel Laptop",
+            "Lenove Laptop",
+            "HP Laptop"});
+            this.metroComboBox1.Location = new System.Drawing.Point(35, 74);
+            this.metroComboBox1.Margin = new System.Windows.Forms.Padding(10);
+            this.metroComboBox1.Name = "metroComboBox1";
+            this.metroComboBox1.Size = new System.Drawing.Size(138, 29);
+            this.metroComboBox1.TabIndex = 0;
+            this.metroComboBox1.UseSelectable = true;
+            this.metroComboBox1.SelectedIndexChanged += new System.EventHandler(this.metroComboBox1_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.AliceBlue;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 185);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(408, 187);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(408, 185);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 452);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Laptop Store ";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private MetroFramework.Controls.MetroComboBox metroComboBox1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroToggle metroToggle1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private MetroFramework.Controls.MetroButton metroButton1;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using MetroFramework;
+using MetroFramework.Forms;
+
+namespace LaptopStore
+{
+    public partial class Form1 : MetroForm
+    {
+        private ILaptop laptop;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //combo box
+        private void metroComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (metroComboBox1.SelectedIndex == 0)
+            {
+                laptop = new Appel();
+                pictureBox1.Image = LaptopStore.Properties.Resources.Apple;
+                if (metroToggle1.Checked)
+                    richTextBox1.Text = laptop.Accept(new DetailedDescription());
+                else
+                    richTextBox1.Text = laptop.Accept(new BriefDescription());
+            }
+            else if (metroComboBox1.SelectedIndex == 1)
+            {
+                laptop = new Lenove();
+                pictureBox1.Image = LaptopStore.Properties.Resources.Lenovo;
+                if (metroToggle1.Checked)
+                    richTextBox1.Text = laptop.Accept(new DetailedDescription());
+                else
+                    richTextBox1.Text = laptop.Accept(new BriefDescription());
+            }
+            else if (metroComboBox1.SelectedIndex == 2)
+            {
+                laptop = new HP();
+                pictureBox1.Image = LaptopStore.Properties.Resources.Hp;
+                if (metroToggle1.Checked)
+                    richTextBox1.Text = laptop.Accept(new DetailedDescription());
+                else
+                    richTextBox1.Text = laptop.Accept(new BriefDescription());
+            }
+        }
+
+        //toggle 
+        private void metroToggle1_CheckedChanged(object sender, EventArgs e)
+        {
+            metroComboBox1_SelectedIndexChanged(null, null);
+        }
+
+        //contributors 
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            MetroMessageBox.Show(this, "Basheer ALMOMANI\n Reed Mryyan", "Visitor Design Pattern Contributers", MessageBoxButtons.OK,MessageBoxIcon.Question,MessageBoxDefaultButton.Button1);
+        }
+    }
+
+
+    #region Laptop Component
+
+    public interface ILaptop
+    {
+        string Name { get; }
+        string Accept(IVisitor v);
+    }
+
+    #region Laptop Types
+
+    public class Lenove : ILaptop
+    {
+        public string Name => "Lenovo Laptop";
+
+        //Bitmap 
+
+        public string Accept(IVisitor v)
+        {
+            return v.Visit(this);
+        }
+    }
+
+    public class Appel : ILaptop
+    {
+        public string Name => "Appel Laptop";
+
+        public string Accept(IVisitor v)
+        {
+            return v.Visit(this);
+        }
+    }
+
+    public class HP : ILaptop
+    {
+        public string Name => "HP Laptop";
+
+        public string Accept(IVisitor v)
+        {
+            return v.Visit(this);
+        }
+    }
+
+    #endregion
+
+    #endregion
+
+
+    #region Visiting Component
+
+    public interface IVisitor
+    {
+        string Visit(ILaptop e);
+    }
+
+    #region Visiting Type
+
+    public class BriefDescription : IVisitor
+    {
+        string IVisitor.Visit(ILaptop e)
+        {
+            if (e is Lenove)
+                return Visit((Lenove) e);
+            if (e is Appel)
+                return Visit((Appel) e);
+            if (e is HP)
+                return Visit((HP) e);
+            return null;
+        }
+
+        private static string Visit(Lenove lenove)
+        {
+            return String.Format("{0}\n{1}\n{2}", " Audio : High Definition (HD) Audio ",
+                "Microprocessor : Intel® Core™ i5 processor i5-520M with dual-core",
+                "Memory : 512MB DDR2 System Memory (2 Dimm)");
+        }
+
+        private static string Visit(Appel appel)
+        {
+            return String.Format("{0}\n{1}\n{2}", " Memory : 4GB of 1600MHz DDR3 memory ",
+                "processor : Intel Core i5 processor (Turbo Boost up to 3.1GHz) with 3MB L3 cach ", " Storage: 500GB");
+        }
+
+        private static string Visit(HP hp)
+        {
+            return String.Format("{0}\n{1}\n{2}", " US Product Number : RG253UA#ABA",
+                "Microprocessor : 2.0 GHz AMD Turion™ 64 Mobile Technology MK-36"
+                , "Memory : 512MB DDR2 System Memory (2 Dimm)");
+        }
+    }
+
+    public class DetailedDescription : IVisitor
+    {
+        public string Visit(ILaptop e)
+        {
+            if (e is Lenove)
+                return Visit((Lenove) e);
+            if (e is Appel)
+                return Visit((Appel) e);
+            if (e is HP)
+                return Visit((HP) e);
+            return null;
+        }
+
+        private static string Visit(Lenove lenove)
+        {
+
+            return String.Format("{0}\n{1}\n{2} \n {3}\n{4}\n{5}\n{6}\n{7}\n{8} ", lenove.Name
+                , "US Product Number : RG253UA#ABA", "Microprocessor : 2.0 GHz AMD Turion™ 64 Mobile Technology MK-36"
+                , "Memory : 512MB DDR2 System Memory (2 Dimm)", "Microprocessor Cache : 512KB L2 Cache ",
+                " Memory Max : 2048MB"
+                , "Video Graphics :NVIDIA GeForce Go 6150 (UMA)", "Hard Drive : 80GB 5400RPM (SATA)", "Weight : 6.8lbs");
+        }
+
+        private static string Visit(Appel appel)
+        {
+            return String.Format("{0}\n{1}\n{2} \n {3}\n{4}\n{5}\n{6} ", appel.Name,
+                "processor : Intel Core i5 processor (Turbo Boost up to 3.1GHz) with 3MB L3 cach "
+                , " Camera : 720p FaceTime HD camera "
+                , " Memory Max : 2048MB",
+                "Video Graphics :NVIDIA GeForce Go 6150 (UMA)",
+                "Hard Drive : 80GB 5400RPM (SATA)",
+                "Weight : 8.6lbs");
+        }
+
+        private static string Visit(HP hp)
+        {
+            return String.Format("{0}\n{1}\n{2} \n {3}\n{4}\n{5}\n{6}\n{7}\n{8} ", hp.Name,
+                " US Product Number : RG253UA#ABA",
+                "Microprocessor : 2.0 GHz AMD Turion™ 64 Mobile Technology MK-36",
+                "Memory : 512MB DDR2 System Memory (2 Dimm)",
+                "Microprocessor Cache : 512KB L2 Cache ",
+                " Memory Max : 2048MB",
+                "Video Graphics :NVIDIA GeForce Go 6150 (UMA)",
+                "Hard Drive : 80GB 5400RPM (SATA)",
+                "Weight : 6.8lbs");
+        }
+    }
+
+    #endregion
+
+    #endregion
+
+
+}
